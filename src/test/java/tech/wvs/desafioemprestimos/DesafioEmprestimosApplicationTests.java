6b7724e890113e92a5361d8a3991bf60c61248bf
wallacevilela7 <wallace.vilela9@gmail.com>
2025-05-21 11:08:20 -0300

    Create Spring app

---
+++ src/test/java/tech/wvs/desafioemprestimos/DesafioEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.desafioemprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioEmprestimosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
